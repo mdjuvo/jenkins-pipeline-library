026c71a2c8e62c053f08d704bfc0a50e6404870b
Michael Davis <mdavis333@yahoo.com>
2019-05-23 13:04:41 -0700

    initial testing

--- vars/pushDockerContainer.groovy
+++ vars/pushDockerContainer.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
-final REGISTRY = "730036231311.dkr.ecr.us-west-2.amazonaws.com"
 
 def call(image, tag) {
+  REGISTRY = "730036231311.dkr.ecr.us-west-2.amazonaws.com"
   docker.withRegistry("https://${REGISTRY}", 'ecr:us-west-2:md-aws-credentials') {
     sh "aws ecr describe-repositories --repository-names ${image} || aws ecr create-repository --repository-name ${image}"
     sh "docker tag ${image}:${tag} ${REGISTRY}/${image}:${tag}"
