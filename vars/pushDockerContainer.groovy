abfe5a5cf4a37b7c2cb57e49222cd7de6f1ac8b4
Michael Davis <mdavis333@yahoo.com>
2019-08-16 11:36:53 -0700

    switch to Artifactory

--- vars/pushDockerContainer.groovy
+++ vars/pushDockerContainer.groovy
@@ -2,15 +2,9 @@
 
 
 def call(image, tag) {
-  REGISTRY = "730036231311.dkr.ecr.us-west-2.amazonaws.com"
-  REGION = "us-west-2"
+  REGISTRY = "registry.juvo.mobi"
 
-  // Automatically create new repos
-  withAWS(region: REGION, credentials: 'md-aws-credentials') {
-    sh "aws --region ${REGION} ecr describe-repositories --repository-names ${image} || aws --region ${REGION} ecr create-repository --repository-name ${image}"
-  }
-
-  docker.withRegistry("https://${REGISTRY}", "ecr:${REGION}:md-aws-credentials") {
+  docker.withRegistry("https://${REGISTRY}", "juvo-registry-credentials") {
     sh "docker tag ${image}:${tag} ${REGISTRY}/${image}:${tag}"
     sh "docker push ${REGISTRY}/${image}:${tag}"
   }
